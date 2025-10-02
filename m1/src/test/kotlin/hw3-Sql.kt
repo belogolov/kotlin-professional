050c80881614c1df7e0b025cb58ab2e98c2a4377
Timur Belogolov <belogolov.timur@yandex.ru>
2025-10-02 09:11:42 +0300

    HWA-3 adds additional homework test

---
+++ m1/src/test/kotlin/hw3-Sql.kt
@@ -0,0 +1,160 @@
+@file:Suppress("unused")
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class SqlSelectBuilder {
+    private var table: String? = null
+    private var columns = listOf<String>()
+    private var where = mutableListOf<String>()
+
+    fun from(table: String) {
+        this.table = table
+    }
+
+    fun select(vararg columns: String) {
+        this.columns = columns.toList()
+    }
+
+    fun where(operation: () -> String) {
+        this.where.add(operation.invoke())
+    }
+
+//    fun or(operation: () -> String) {
+//        this.where.add(operation.invoke())
+//    }
+//
+//    fun wheres(block: SqlWhereBuilder.() -> Unit): SqlWhereBuilder =
+//        SqlWhereBuilder().apply(block)
+
+    fun build(): String {
+        requireNotNull(table) { "Table cannot be null" }
+        val col = columns.takeIf { it.isNotEmpty() }?.joinToString(separator = ", ") ?: "*"
+        val where = where.takeIf { it.isNotEmpty() }?.joinToString(separator = " or ")?.let { " where $it" } ?: ""
+        return "select $col from $table$where"
+    }
+
+//    class SqlWhereBuilder {
+//        private var ors: MutableList<String> = mutableListOf()
+//
+//        fun or(operation: () -> String) {
+//            ors.add(operation.invoke())
+//        }
+//
+//        fun getOrs() = ors.takeIf { it.isNotEmpty() }?.joinToString(separator = " or ")?.let { " where $it" } ?: ""
+//    }
+}
+
+
+private infix fun String.eq(value: Any?): String {
+    if (value == null) return "$this is null"
+    return "$this = '$value'"
+}
+
+private infix fun String.nonEq(value: Any?): String {
+    if (value == null) return "$this !is null"
+    return "$this != $value"
+}
+
+
+fun query(block: SqlSelectBuilder.() -> Unit): SqlSelectBuilder =
+    SqlSelectBuilder().apply(block)
+
+
+// Реализуйте dsl для составления sql запроса, чтобы все тесты стали зелеными.
+class Hw1Sql {
+
+    private fun checkSQL(expected: String, sql: SqlSelectBuilder) {
+        assertEquals(expected, sql.build())
+    }
+
+    @Test
+    fun `simple select all from table`() {
+        val expected = "select * from table"
+
+        val real = query {
+            from("table")
+        }
+
+        checkSQL(expected, real)
+    }
+
+    @Test
+    fun `check that select can't be used without table`() {
+        assertFailsWith<Exception> {
+            query {
+                select("col_a")
+            }.build()
+        }
+    }
+
+    @Test
+    fun `select certain columns from table`() {
+        val expected = "select col_a, col_b from table"
+
+        val real = query {
+            select("col_a", "col_b")
+            from("table")
+        }
+
+        checkSQL(expected, real)
+    }
+
+    /**
+     * __eq__ is "equals" function. Must be one of char:
+     *  - for strings - "="
+     *  - for numbers - "="
+     *  - for null - "is"
+     */
+    @Test
+    fun `select with complex where condition with one condition`() {
+        val expected = "select * from table where col_a = 'id'"
+
+        val real = query {
+            from("table")
+            where { "col_a" eq "id" }
+        }
+
+        checkSQL(expected, real)
+    }
+
+    /**
+     * __nonEq__ is "non equals" function. Must be one of chars:
+     *  - for strings - "!="
+     *  - for numbers - "!="
+     *  - for null - "!is"
+     */
+    @Test
+    fun `select with complex where condition with two conditions`() {
+        val expected = "select * from table where col_a != 0"
+
+        val real = query {
+            from("table")
+            where {
+                "col_a" nonEq 0
+            }
+        }
+
+        checkSQL(expected, real)
+    }
+
+    @Test
+    fun `when 'or' conditions are specified then they are respected`() {
+//        val expected = "select * from table where (col_a = 4 or col_b !is null)"
+//
+//        val real = query {
+//            from("table")
+//            where {
+//                or {
+//                    "col_a" eq 4
+//                    "col_b" nonEq null
+//                }
+//            }
+//        }
+//
+//        checkSQL(expected, real)
+    }
+}
+
+
